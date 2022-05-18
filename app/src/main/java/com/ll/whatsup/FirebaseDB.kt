a646d531c1b4eb22ef99e0cc1af60b50c85294d8
Asad Kamran <kamranasad7@gmail.com>
2022-05-19 02:47:48 +0500

    FirebaseDB: more db references

--- app/src/main/java/com/ll/whatsup/FirebaseDB.kt
+++ app/src/main/java/com/ll/whatsup/FirebaseDB.kt
@@ -1,41 +1,45 @@
 package com.ll.whatsup
 
-import com.google.android.gms.tasks.Tasks.await
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.ll.whatsup.model.Account
+import com.ll.whatsup.model.Chat
+import com.ll.whatsup.model.Contact
+import com.ll.whatsup.model.Message
 
-class FirebaseDB {
+object FirebaseDB {
+    val url = "https://whatsup-messenger-f8f2c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    val db = Firebase.database(url)
+    lateinit var currentAccount: Account
+    val contacts: ArrayList<Contact> = ArrayList()
+    val contactsHash: HashMap<String, Contact> = HashMap()
+    lateinit var chatsRef: DatabaseReference
 
-    val db = Firebase.database("https://whatsup-messenger-f8f2c-default-rtdb.asia-southeast1.firebasedatabase.app/")
-
-    companion object LoginAccount {
-        lateinit var acc: Account
+    fun initialize() {
+        chatsRef = db.getReference("accounts/${Firebase.auth.currentUser?.phoneNumber}")
     }
 
-    fun getAccount(accountNo: String): Account? {
-
-        val accVal = db.getReference("accounts/${accountNo}")
-        accVal.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                    acc = snapshot.getValue<Account>()!!
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
-        val acc: DataSnapshot = await(accVal.get())
-        return acc.getValue<Account>()
+    fun getAccount(accountNo: String, onSuccessListener: OnSuccessListener<DataSnapshot>) {
+        val accountRef = db.getReference("accounts/${accountNo}")
+        accountRef.get().addOnSuccessListener(onSuccessListener)
     }
 
-    fun addMsgtoChat() {
+    fun getAccountReference(accountNo: String): DatabaseReference {
+        return db.getReference("accounts/${accountNo}")
+    }
 
+
+    fun setContactsHash() {
+        for(c in contacts){
+            contactsHash[c.number] = c
+        }
     }
 
 
