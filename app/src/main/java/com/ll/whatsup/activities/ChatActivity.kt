b62f74090cc869087970be9aa0f0b633ba957fce
Asad Kamran <kamranasad7@gmail.com>
2022-05-19 02:46:06 +0500

    Heterogeneous Chat  Adapter

--- app/src/main/java/com/ll/whatsup/activities/ChatActivity.kt
+++ app/src/main/java/com/ll/whatsup/activities/ChatActivity.kt
@@ -1,5 +1,6 @@
 package com.ll.whatsup.activities
 
+import android.app.Dialog
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
@@ -10,6 +11,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -28,98 +30,120 @@ import com.google.gson.Gson
 import com.ll.whatsup.R
 import com.ll.whatsup.adapter.ChatAdapter
 import com.ll.whatsup.model.Chat
+import com.ll.whatsup.model.Contact
 
 class ChatActivity() : AppCompatActivity() {
 
-    lateinit var chat:Chat
-    lateinit var testTXT: TextView
+    var chat: Chat? = null
+    var contact: Contact? = null
     lateinit var msgText: EditText
-    val db = Firebase.database("https://whatsup-messenger-f8f2c-default-rtdb.asia-southeast1.firebasedatabase.app/")
-    val testValRef: DatabaseReference = db.getReference("test-message")
+    lateinit var dialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val gson=Gson()
-        chat = gson.fromJson<Chat>(intent.getStringExtra("ChatwithContact"), Chat::class.java)
+        val gson = Gson()
+        chat = gson.fromJson(intent.getStringExtra("chat"), Chat::class.java)
+        contact = gson.fromJson(intent.getStringExtra("contact"), Contact::class.java)
 
-        setTitle(chat.chatName)
 
-        testTXT = findViewById(R.id.testTXT)
+        if(contact != null){
+            title = contact?.name
+        }
+        else if(chat != null){
+            title = chat?.accountNum
+        }
+
+
         msgText = findViewById(R.id.inputMsg)
         findViewById<Button>(R.id.sendBtn).setOnClickListener { sendMessage() }
 
-        testValRef.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                testTXT.text = snapshot.getValue<String>()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(applicationContext, error.message, Toast.LENGTH_LONG).show()
-            }
-
-        })
         displayChat()
     }
 
     private fun sendMessage() {
-        testValRef.setValue(msgText.text.toString())
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-        val inf:MenuInflater = menuInflater
+        val inf: MenuInflater = menuInflater
         inf.inflate(R.menu.chat_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if(item.itemId == R.id.call_menu){
-            if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this,Array(1){android.Manifest.permission.CALL_PHONE},110)
-            }
-            else{
-                val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
-                    applicationContext.registerReceiver(null, ifilter)
-                }
+        if (item.itemId == R.id.call_menu) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    Array(1) { android.Manifest.permission.CALL_PHONE },
+                    110
+                )
+            } else {
+                val batteryStatus: Intent? =
+                    IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+                        applicationContext.registerReceiver(null, ifilter)
+                    }
                 val batteryPct: Float? = batteryStatus?.let { intent ->
                     val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
                     val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
                     level * 100 / scale.toFloat()
                 }
                 if (batteryPct != null) {
-                    if(batteryPct < 20)
-                        Toast.makeText(applicationContext,"Battery is too low to make call", Toast.LENGTH_LONG).show()
-                    else{
-                        val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:03004031900"))
+                    if (batteryPct < 20)
+                        Toast.makeText(
+                            applicationContext,
+                            "Battery is too low to make call",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    else {
+                        val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:${chat?.accountNum}"))
                         startActivity(i)
                     }
-                }
-                else{
-                    val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:03004031900"))
+                } else {
+                    val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:${chat?.accountNum}"))
                     startActivity(i)
                 }
             }
-
         }
         return true
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == 110 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:03004031900"))
+        if (requestCode == 110 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val i = Intent(Intent.ACTION_CALL, Uri.parse("tel:${chat?.accountNum}"))
             startActivity(i)
         }
     }
 
-    private fun displayChat(){
-        val adp= ChatAdapter(chat)
+    private fun displayChat() {
+        lateinit var adp: ChatAdapter
+
+        if(chat != null){
+            adp = ChatAdapter(chat!!)
+        }
+        else if(contact != null) {
+            adp = ChatAdapter(Chat(contact!!.number))
+        }
+        else {
+            adp = ChatAdapter(Chat())
+            Toast.makeText(this, "Chat ot contact not found", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
         val view = findViewById<RecyclerView>(R.id.chatView)
         view.layoutManager = LinearLayoutManager(this)
-        view.adapter=adp
+        view.adapter = adp
     }
 }
--- app/src/main/java/com/ll/whatsup/adapter/ChatAdapter.kt
+++ app/src/main/java/com/ll/whatsup/adapter/ChatAdapter.kt
@@ -5,29 +5,70 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ll.whatsup.FirebaseDB
 import com.ll.whatsup.R
 import com.ll.whatsup.model.Chat
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ChatAdapter(var chat : Chat) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+class ChatAdapter(var chat : Chat) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    class ChatViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        var msg = itemView.findViewById<TextView>(R.id.text_message_body)
-        var msg_time = itemView.findViewById<TextView>(R.id.text_message_time)
+    private val SENDER = 0
+    private val RECEIVER = 1
+
+    class SenderMessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var msg: TextView = itemView.findViewById(R.id.text_message_body)
+        var msgTime: TextView = itemView.findViewById(R.id.text_message_time)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatAdapter.ChatViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.chat_send_card,parent,false)
-        return ChatViewHolder(v)
+    class ReceiverMessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var msg = itemView.findViewById<TextView>(R.id.text_message_body_rcv)
+        var msgTime = itemView.findViewById<TextView>(R.id.text_message_time_rcv)
     }
 
-    override fun onBindViewHolder(holder: ChatAdapter.ChatViewHolder, position: Int) {
-        val txt = chat.messages[position]
-        holder.msg.text = txt.text
-        holder.msg_time.text = txt.time.toString()
+    override fun getItemViewType(position: Int): Int {
+        return if(chat.messages[position].senderNum == FirebaseDB.currentAccount.number)  SENDER else RECEIVER
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            SENDER -> {
+                val v = LayoutInflater.from(parent.context).inflate(R.layout.chat_send_card,parent,false)
+                SenderMessageViewHolder(v)
+            }
+
+            RECEIVER -> {
+                val v = LayoutInflater.from(parent.context).inflate(R.layout.chat_recieve_card,parent,false)
+                ReceiverMessageViewHolder(v)
+            }
+            else -> {
+                val v = LayoutInflater.from(parent.context).inflate(R.layout.chat_recieve_card,parent,false)
+                ReceiverMessageViewHolder(v)
+            }
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder.itemViewType){
+            SENDER -> {
+                val senderHolder = holder as SenderMessageViewHolder
+                val message = chat.messages[position]
+                holder.msg.text = message.text
+                val dateFormat = SimpleDateFormat("hh.mm aa", Locale.US)
+                holder.msgTime.text = dateFormat.format(message.time).toString()
+            }
+            RECEIVER -> {
+                val receiverHolder = holder as ReceiverMessageViewHolder
+                val message = chat.messages[position]
+                holder.msg.text = message.text
+                val dateFormat = SimpleDateFormat("hh.mm aa", Locale.US)
+                holder.msgTime.text = dateFormat.format(message.time).toString()
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return chat.messages.size
     }
-
 }
